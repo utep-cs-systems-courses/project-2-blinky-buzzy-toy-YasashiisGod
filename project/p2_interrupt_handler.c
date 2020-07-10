d3853c2002f58a8423b09712e37871df3be27ad9
YasashiisGod <secretlyytheflash@gmail.com>
2020-07-09 21:53:52 -0600

    BLINKED FROM TOP WOOOOO

---
+++ project/p2_interrupt_handler.c
@@ -0,0 +1,12 @@
+#include <msp430.h>
+#include "switches.h"
+
+/* Switch on P2 (S2) */
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & BUTTONS) {	      /* did a button cause this interrupt? */
+    P2IFG &= ~BUTTONS;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
+}
+
